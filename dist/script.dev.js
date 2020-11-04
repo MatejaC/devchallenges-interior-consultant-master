f3e9ba9162fec9895e700f98568801715f6501aa
Mateja Cimpersak <matejacimpersak@gmail.com>
2020-11-04 15:23:10 +0100

    adding new js script for mobile slider navigation; mixing flex annd grid components for better responsive styling; tablet mode finished

---
+++ dist/script.dev.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var burger = document.getElementById('burger');
+var ul = document.querySelector('nav ul');
+burger.addEventListener("click", function () {
+  burger.classList.toggle('show-x');
+  ul.classList.toggle('show');
+});
