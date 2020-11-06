0d9711c5a2ea10b3a140188effb464cbc36c9314
Mateja Cimpersak <matejacimpersak@gmail.com>
2020-11-06 01:26:46 +0100

    removing old js script

--- dist/script.dev.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-var burger = document.getElementById('burger');
-var ul = document.querySelector('nav ul');
-burger.addEventListener("click", function () {
-  burger.classList.toggle('show-x');
-  ul.classList.toggle('show');
-});
